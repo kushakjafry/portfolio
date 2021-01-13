fa7912a21bfb74e0f6e9e43370fe8f3056011f6e
kushak jafry <kushakjafry@gmail.com>
2021-01-13 11:51:29 +0530

    added meta tags

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,7 @@ import {
   Renderer2,
   ViewChild,
 } from '@angular/core';
+import { Meta } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
@@ -12,18 +13,22 @@ import {
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  constructor(private renderer: Renderer2) {}
+  constructor(private renderer: Renderer2, private meta: Meta) {
+    this.meta.addTag({
+      name: 'description',
+      content: 'My personal portfolio.',
+    });
+    this.meta.addTag({ name: 'author', content: 'Kushak Jafry' });
+    this.meta.addTag({
+      name: 'keywords',
+      content: 'Portfolio, Angular, Strapi',
+    });
+  }
   title = 'portfolio';
   @ViewChild('scrollbar') scrollBar: ElementRef;
   @ViewChild('header') header: ElementRef;
   @HostListener('window:scroll', ['$event'])
   onScroll($event) {
-    // this.renderer.setStyle(this.header.nativeElement, 'padding', '1em 0');
-    // this.renderer.setStyle(
-    //   this.header.nativeElement,
-    //   'background-color',
-    //   'black'
-    // );
     this.renderer.addClass(this.header.nativeElement, 'scrolled');
     this.renderer.setStyle(
       this.scrollBar.nativeElement,
@@ -35,14 +40,6 @@ export class AppComponent {
       }%`
     );
     if (window.pageYOffset === 0) {
-      // this.renderer.setStyle(
-      //   this.header.nativeElement,
-      //   'background-color',
-      //   'transparent'
-      // );
-      // this.renderer.setStyle(
-      //   this.header
-      // )
       this.renderer.removeClass(this.header.nativeElement, 'scrolled');
     }
   }
